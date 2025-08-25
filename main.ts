1ab9bae5281db8e11a79f2ebc051701f77030dd1
c-m-stone <matthew.steele@cornermilestone.com>
2025-08-25 14:38:42 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { latLngToCell } from "https://esm.sh/h3-js@3.7.2";
+import h3 from "https://esm.sh/h3-js@3.7.2";
 
 Deno.serve(async (req) => {
   const url = new URL(req.url);
@@ -10,8 +10,8 @@ Deno.serve(async (req) => {
     return new Response("Missing or invalid lat/lng/res", { status: 400 });
   }
 
-  const h3 = latLngToCell(lat, lng, res);
-  return new Response(JSON.stringify({ h3 }), {
+  const h3Index = h3.latLngToCell(lat, lng, res);
+  return new Response(JSON.stringify({ h3: h3Index }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   });
